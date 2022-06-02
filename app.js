e8780c82f8271713ae2d8b2eaa089dc6229ae960
jinseok1006 <jinseok1006@naver.com>
2022-06-03 01:04:17 +0900

    Updates

---
+++ app.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+function draw() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            ctx.save();
+            ctx.fillStyle = `rgb(${51 * i},${255 - 51 * i},255)`;
+            ctx.translate(10 + j * 50, 10 + i * 50);
+            ctx.fillRect(0, 0, 25, 25);
+            ctx.restore();
+        }
+    }
+}
+
+draw();
+const size = 10;
+const [x, y] = [10, 10];
